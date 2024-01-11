1034aed10e509c8105f7382c23762eacc68cb007
Andrew Dobson <tandrewlopez@gmail.com>
2024-01-11 14:13:45 -0500

    made data table sortable

--- app/(admin)/submissions/_components/columns.tsx
+++ app/(admin)/submissions/_components/columns.tsx
@@ -1,30 +1,87 @@
 "use client";
 
-import { InitialSubmission } from "@/temporary/types";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { InitialSubmission } from "@/temporary/types";
 
 export const columns: ColumnDef<InitialSubmission>[] = [
     {
         accessorKey: "name",
-        header: "Name",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Name
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
     },
     {
         accessorKey: "email",
-        header: "Email",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Email
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
     },
     {
         accessorKey: "phoneNumber",
         header: "Phone Number",
         cell: ({ row }) => {
-            const phoneNumber: string = row.getValue('phoneNumber')
-            const areaCode = phoneNumber.slice(0, 3)
-            const centralCode = phoneNumber.slice(3, 6)
-            const lineNumber = phoneNumber.slice(6)
-            return `(${areaCode}) ${centralCode}-${lineNumber}`
-        }
+            const phoneNumber: string = row.getValue("phoneNumber");
+            const areaCode = phoneNumber.slice(0, 3);
+            const centralCode = phoneNumber.slice(3, 6);
+            const lineNumber = phoneNumber.slice(6);
+            return `(${areaCode}) ${centralCode}-${lineNumber}`;
+        },
     },
     {
         accessorKey: "description",
         header: "Description",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const submission = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open Menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                            className="cursor-pointer"
+                            onClick={() => navigator.clipboard.writeText(submission.id)}>
+                            Copy Submission ID
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>View Submission</DropdownMenuItem>
+                        <DropdownMenuItem>Something Else</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            );
+        },
     },
 ];
