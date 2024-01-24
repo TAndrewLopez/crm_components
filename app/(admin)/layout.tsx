9ca3ba202ab15df20af0fca885cf14ffe02942f8
Andrew Dobson <tandrewlopez@gmail.com>
2024-01-24 15:22:33 -0500

    something broke

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,6 +1,9 @@
 import { Sidebar, SidebarSkeleton } from "@/components/sidebar";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 type Props = {
     children: React.ReactNode;
 };
